7dcbbe3f2e7cb943210581276d0828f102533cf3
Tay <tay.james.d@gmail.com>
2019-11-03 19:20:36 -0700

    updates url patterns

--- olympic_tracker/olympians/urls.py
+++ olympic_tracker/olympians/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     r'^api/v1/olympians',
     views.get_olympians,
     name='get_olympians'
+    ),
+    url(
+    r'api/v1/olympian_stats',
+    views.olympian_stats,
+    name='olympian_stats'
     )
 ]
