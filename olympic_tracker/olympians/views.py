cdc96ae6d8d67bbea1e680903db8282c660896c6
Tay <tay.james.d@gmail.com>
2019-11-03 19:20:21 -0700

    Creates olympian_stats method that analyzes olympian data

--- olympic_tracker/olympians/views.py
+++ olympic_tracker/olympians/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Olympian
 from .serializers import OlympianSerializer
+from django.db import connection
 
 @api_view(['GET'])
 def get_olympians(request):
@@ -20,5 +21,40 @@ def get_olympians(request):
             oldest = Olympian.objects.raw('SELECT * FROM public.olympians_olympian ORDER BY age desc LIMIT 1')
             oldest_serializer = OlympianSerializer(oldest, many=True)
             return Response(oldest_serializer.data)
+
+@api_view(['GET'])
+def olympian_stats(request):
+    olympians = Olympian.objects.all()
+    male_weight = "SELECT AVG(CAST(weight as int)) FROM public.olympians_olympian WHERE weight ~ '^[0-9]+$' and sex='M';"
+    female_weight = "SELECT AVG(CAST(weight as int)) FROM public.olympians_olympian WHERE weight ~ '^[0-9]+$' and sex='F';"
+    total_competing_olympians = "SELECT COUNT(DISTINCT name) FROM public.olympians_olympian"
+    average_age = "SELECT AVG(CAST(age as int)) FROM public.olympians_olympian WHERE age ~ '^[0-9]+$';"
+
+    cursor = connection.cursor()
+
+    result1 = cursor.execute(male_weight)
+    fetch1 = cursor.fetchone()[0]
+
+    result2 = cursor.execute(female_weight)
+    fetch2 = cursor.fetchone()[0]
+
+    result3 = cursor.execute(total_competing_olympians)
+    fetch3 = cursor.fetchone()[0]
+
+    result4 = cursor.execute(average_age)
+    fetch4 = cursor.fetchone()[0]
+
+    average_weights = {"unit": "kg",
+        "male_olympians": fetch1,
+        "female_olympians": fetch2
+    }
+
+    stats = {
+        "total_competing_olympians": fetch3,
+        "average_weight": average_weights,
+        "average_age": fetch4
+    }
+
+    return Response(stats)
 
 # Create your views here.
